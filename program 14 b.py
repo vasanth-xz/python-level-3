eca3c15ec19fe19826ccf9d069a4513b711b82f5
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-09-29 10:12:07 +0530

    Add files via upload

---
+++ program 14 b.py
@@ -0,0 +1,32 @@
+for i in range(10):
+    print(i,end=' ')
+
+
+print("for program 2")
+for i in range(101):
+    print(i, end=' ')
+
+
+print("for program 3")
+for i in range(50,101):
+    print(i,end=' ')
+
+
+print("for program 4")
+for i in range(5,501,5):
+    print(i,end=' ')
+
+
+print("for program 5")
+for i in range(100,151,2):
+    print(i,end=' ')
+
+
+print("for program 6")
+for i in range(10,0,-1):
+    print(i,end=' ')
+
+
+print("for program 7")
+for i in range(100,9,-5):
+    print(i,end=' ')
