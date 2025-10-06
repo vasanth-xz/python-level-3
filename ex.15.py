9a7c15cbb02727d4d87fd8b3ae16f44d8d4638ea
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-10-06 10:45:33 +0530

    Add files via upload

---
+++ ex.15.py
@@ -0,0 +1,7 @@
+print("Table Program")
+print("--------------")
+n=int(input("Enter the Number"))
+print("Table")
+print("-------------------------")
+for i in range(1,11):
+    print(str(i)+"x"+str(n)+"-"+str(i*n))
